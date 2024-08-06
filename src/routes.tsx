dfe02d52470bebf172107279c67f004b81d1b363
yucuecbdisubvrvnfkdsjb <163767554+yucuecbdisubvrvnfkdsjb@users.noreply.github.com>
2024-08-06 15:34:56 -0400

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,7 +6,7 @@ import { ProxyFrame } from "./pages/ProxyFrame.js";
 import { Radon } from "./pages/Radon";
 import { Settings } from "./pages/Settings/";
 import { AboutBlank } from "./AboutBlank";
-import { Faq } from "./pages/Faq";
+
 // import { registerRemoteListener } from "@mercuryworkshop/bare-mux";
 import "./style.css";
 import "./i18n";
